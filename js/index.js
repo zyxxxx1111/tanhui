140f916aeea20669469a0b28a3da5a1abaa06363
zyxxxx1111 <131589961+zyxxxx1111@users.noreply.github.com>
2023-04-26 23:54:34 +0800

    Create index.js

--- js/index.js
+++ js/index.js
@@ -289,7 +289,7 @@
         textStyle:{
           //color:'#4c9bfd'
           color:'#FFFAFA',
-          fontSize:'11.5px'
+          fontSize:'10px'
         },
         left:'0%',
         top:"-1%"
