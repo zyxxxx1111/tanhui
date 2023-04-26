45bdfa32299f02afa27864fb44eb38fdcde5031e
zyxxxx1111 <131589961+zyxxxx1111@users.noreply.github.com>
2023-04-26 23:27:38 +0800

    Add files via upload

---
+++ js/Brazil.js
@@ -0,0 +1,12 @@
+document.querySelector(".caida").addEventListener("click", function () {
+    // document.querySelector("#ddList").setAttribute("style", "display: block");
+    $('#ddList').toggle();
+  });
+  document.querySelector(".caidan").addEventListener("click", function () {
+    // document.querySelector("#ddList").setAttribute("style", "display: block");
+    $('#dddList').toggle();
+  });
+  document.querySelector(".caidann").addEventListener("click", function () {
+    // document.querySelector("#ddList").setAttribute("style", "display: block");
+    $('#ddddList').toggle();
+  });
